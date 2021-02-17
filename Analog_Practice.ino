129cb277d55f723a610bda5215c0e58a93757d28
Genevieve Goebel <41485395+genevievegoebel@users.noreply.github.com>
2021-02-16 20:05:23 -0500

    Analog_ORP_Trial
    Commented out demo code lines to simplify, print values to serial monitor, delay for 1 reading/second.

--- Analog_Practice.ino
+++ Analog_Practice.ino
@@ -1,8 +1,8 @@
 
 int potPin = 0;
-int latchPin = 5;
-int clockPin = 6;
-int dataPin = 4;
+//int latchPin = 5;
+//int clockPin = 6;
+//int dataPin = 4;
  
 int oxredPot = 0;
 
@@ -12,32 +12,30 @@ int oxredPot = 0;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(latchPin, OUTPUT);
-  pinMode(dataPin, OUTPUT);  
-  pinMode(clockPin, OUTPUT);
+  //pinMode(latchPin, OUTPUT);
+  //pinMode(dataPin, OUTPUT);  
+  //pinMode(clockPin, OUTPUT);
 }
 
 void loop() {
   //This line is used to read in analog values from A0
   int reading = analogRead(potPin);
-  int oxredPot = reading / 114;  //1023 / 9
-  //for (int i = 0; i < numLEDSLit; i++)
-  //{
-  //  bitSet(leds, i); 
-  //}
-  updateShiftRegister();
+  //int oxredPot = reading / 114;  //1023 / 9
+ Serial.println(reading);
+ delay(1000);
+  //updateShiftRegister();
 }
 
 //This function sets the latchPin to low, calls shiftOut() before
 //setting it back to high
 //shiftOut([data], [clock], [start point data], [actual data])
-void updateShiftRegister()
-{
-   digitalWrite(latchPin, LOW);
+//void updateShiftRegister()
+//{
+   //digitalWrite(latchPin, LOW);
    //Least significant bit (LSBFIRST)
-   shiftOut(dataPin, clockPin, LSBFIRST, oxredPot);
-   digitalWrite(latchPin, HIGH);
-}
+   //shiftOut(dataPin, clockPin, LSBFIRST, oxredPot);
+   //digitalWrite(latchPin, HIGH);
+//}
 
 //Use of variable 10 kOhm resistor (potentiometer aka. pot) 
 //connected to the Arduino board allows use of knob to change the 
